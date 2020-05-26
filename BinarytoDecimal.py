89944254be05e928e59bac45b088f19b519e7d77
Lavanyaa-b <65484102+Lavanyaa-b@users.noreply.github.com>
2020-05-26 19:45:58 +0530

    Add files via upload

---
+++ BinarytoDecimal.py
@@ -0,0 +1,7 @@
+#github repository- Lavanya-B
+
+
+for i in range(1111112):
+   
+   if set(str(i)).issubset({"0", "1"}):
+     print(f"{i:7d} => {int(str(i), 2):3d}")
---
+++ counting.py
@@ -0,0 +1,4 @@
+#guithub repository- Lavanya-B
+
+for n in range (0,10001, 5):
+      print(n)
