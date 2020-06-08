259be62a3d9b461848fca07077fe5e03b907e12f
Lavanyaa-b <65484102+Lavanyaa-b@users.noreply.github.com>
2020-06-08 19:00:32 +0530

    Add files via upload

---
+++ Helloword.java
@@ -0,0 +1,7 @@
+/* Github repository- Lavanya-B */
+
+class MyClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ Sum.java
@@ -0,0 +1,14 @@
+/* Github repository- Lavanya-B */
+
+class addition{
+
+     public static void main (String[]args){
+        int Z=sum(74,36);
+        System.out.println(Z);
+    }
+
+    static int sum(int a,int b){
+        return a+b;
+    }
+    
+}
