bcb80cae201727b49c46804b8ff4aad1cb4dc2bb
Lavanya Basavaraju <65484102+Lavanyaa-b@users.noreply.github.com>
2020-06-09 18:51:20 +0530

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,9 @@
+public class Program {
+    public static void main(String[] args) {
+        int x = 1;
+        do {
+            System.out.println(x);
+            x++;
+        } while(x < 0);
+    }
+}
---
+++ forloop.java
@@ -0,0 +1,9 @@
+// Github repository - Lavanya-B
+
+public class Program {
+    public static void main(String[] args) {
+        for(int x = 1; x <=5; x++) {
+            System.out.println(x);
+        }
+    }
+}
