1672e760e0af8f4017f1b79c1d0d31893b052e37
Lavanya Basavaraju <65484102+Lavanyaa-b@users.noreply.github.com>
2020-06-13 19:33:18 +0530

    Add files via upload

---
+++ Anonymous.java
@@ -0,0 +1,17 @@
+class Machine {
+    public void start() {
+        System.out.println("Starting...");
+    }
+}
+
+class Program {
+    public static void main(String[ ] args) {
+        Machine m1 = new Machine() {
+            @Override public void start() {
+                System.out.println("Wooooo");
+            }
+        };
+        Machine m2 = new Machine();
+        m2.start();
+    }
+}
