1c4b3ce43d2461825f197a2632bccb2e9d9bfd0e
Lavanya Basavaraju <65484102+Lavanyaa-b@users.noreply.github.com>
2020-06-15 20:45:51 +0530

    Add files via upload

---
+++ Threads.java
@@ -0,0 +1,13 @@
+class Loader implements Runnable {
+    public void run() {
+        System.out.println("Hello");
+    }
+}
+
+class MyClass {
+    public static void main(String[ ] args) {
+        Thread t = new Thread(new Loader());
+        t.start();
+    }
+}
+
